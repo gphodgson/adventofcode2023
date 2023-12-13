8ac15838725b18c1a14a9663814c67130faacaa8
Geoff <geoff.hodgson@priceline.com>
2023-12-13 16:12:12 -0600

    commit 13/12/2023412PM

--- day_02/main.py
+++ day_02/main.py
@@ -5,46 +5,43 @@ GREEN_MAX = 13
 BLUE_MAX = 14
 
 
-def getIdSummation(input_file:str)->int:
-    idSummation = 0
+def getGamePower(input_file:str)->int:
+    totalPower = 0
     with(fileinput.input(files=input_file, encoding="utf-8")) as f:
         for line in f:
-            print("------")
             gameValid = True
             gameIdIndex = line.find(":")
             gameId = int(line[gameIdIndex-2] + line[gameIdIndex-1])
             gameData = line[gameIdIndex+2:].replace("\n", "").split("; ")
+            power = 0
+
+            redMax = 0;
+            greenMax = 0;
+            blueMax = 0
+
             for game in gameData:
-                print(game)
                 handFull = game.split(", ")
                 for balls in handFull:
                     ball = balls.split(" ")
                     amount = int(ball[0])
                     match ball[1]:
                         case "red":
-                            if (amount > RED_MAX):
-                                gameValid = False
-                                break
+                            if (amount > redMax):
+                                redMax = amount
                         case "blue":
-                            if (amount > BLUE_MAX):
-                                gameValid = False
-                                break
+                            if (amount > blueMax):
+                                blueMax = amount
                         case "green":
-                            if (amount > GREEN_MAX):
-                                gameValid = False
-                                break
-                if(not gameValid):
-                   print("bad game")
-                   break
-
-            if(gameValid):
-                idSummation += gameId
+                            if (amount > greenMax):
+                                greenMax = amount
                 
+            power = redMax * greenMax * blueMax
+            totalPower += power
 
-    return idSummation;
+    return totalPower;
 
 def main():
-    print(getIdSummation("input.txt"))
+    print(getGamePower("input.txt"))
 
 if __name__ == "__main__":
     main()
